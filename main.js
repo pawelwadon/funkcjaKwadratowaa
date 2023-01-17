ed8a18f5d40e96db4752fccf2daa7271ee573912
pawelwadon <115095812+pawelwadon@users.noreply.github.com>
2023-01-17 15:05:34 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+const wyniki = document.querySelector('.wyniki')
 const delta = document.querySelector('#delta');
 const postacO = document.querySelector('#postacO');
 const postacK = document.querySelector('#postacK');
@@ -8,180 +9,133 @@ const przeciecieOY = document.querySelector('#oy');
 const oblButton = document.querySelector(".oblicz");
 const wrcButton = document.querySelector(".wrocPrzycisk")
 
-// let wsplA = document.getElementById('wspolczynnikA').value;
-// let wsplB = document.getElementById('wspolczynnikB').value;
-// let wsplC = document.getElementById('wspolczynnikC').value;
+const wsplA = document.getElementById('wspolczynnikA');
+const wsplB = document.getElementById('wspolczynnikB');
+const wsplC = document.getElementById('wspolczynnikC');
 
 
 function obliczeniaFunkcji(){
-const a = document.getElementById('wspolczynnikA').value;
-const b = document.getElementById('wspolczynnikB').value;
-const c = document.getElementById('wspolczynnikC').value;
+const a = wsplA.value;
+const b = wsplB.value;
+const c = wsplC.value;
+
 
 let deltaF = b*b-(4*a*c);
-let deltaFP = Math.sqrt(deltaF)
+let deltaFP = Math.sqrt(deltaF);
 
 let x1 = (-b - deltaFP )/(2*a);
 let x2 = (-b + deltaFP)/(2*a);
 let x0 = -b/(2*a);
 
-const p = -b/(2*a)
-const q = -deltaF/(4*a)
+const p = -b/(2*a);
+const q = -deltaF/(4*a);
 
 
-    if(a ===''){
-        deltaElement = document.createElement('p');
-        deltaElement.innerHTML = `Funkja nie jest kwadratowa`;
-        deltaElement.classList.add('wynikiObl');
-        delta.appendChild(deltaElement);
+    if(a =='' || a==0){
+        dodajPole(`Postać ogólna funkcji`,`f(x) = ${b}x + ${c}`,postacO);
 
-        const postacOgl = document.createElement('p');
-        postacOgl.innerHTML = `f(x) = ${b}x + ${c}`
-        postacOgl.classList.add('wynikiObl');
-        postacO.appendChild(postacOgl);
+        dodajPole(`Miejsce przecięcia osi OX`,`Funkcja przecina oś OX w punkcie (${(-c)/(b)},0)`,miejscaZerowe)
 
-        const postacKan = document.createElement('p');
-        postacKan.innerHTML = `Funkcja nie ma postaci kanonicznej`
-        postacKan.classList.add('wynikiObl');
-        postacK.appendChild(postacKan);
+        dodajPole(`Miejsce przecięcia osi OY`,`Funkcja przecina oś OY w punkcie (0, ${c})`,przeciecieOY);
 
-        const postacIlo = document.createElement('p');
-        postacIlo.innerHTML = `Funkcja nie ma postaci iloczynowej`
-        postacIlo.classList.add('wynikiObl');
-        postacI.appendChild(postacIlo);
+    }else if( a != 0 && deltaF>0){
+        dodajPole(`Postać ogólna funkcji`,`f(x) = ${a}x ${'<sup>2</sup>'} + ${b}x + ${c}`,postacO);
 
-        const oY = document.createElement('p');
-        oY.innerHTML = `Funkcja przecina oś OY w punkcie (0, ${c})`
-        oY.classList.add('wynikiObl');
-        przeciecieOY.appendChild(oY);
+        dodajPole(`Postać kanoniczna funkcji`,`f(x) = ${a}(x - (${p}) )${'<sup>2</sup>'} + (${q})`,postacK);
 
+        dodajPole(`Postać iloczynowa funkcji`,`f(x) = ${a}(x - (${x1}))*(x - (${x2}))`,postacI);
 
-    }else if( a !== "" && deltaF>0){
+        dodajPole(`Delta`,deltaF,delta);
 
-        const deltaElement = document.createElement('p');
-        deltaElement.innerHTML = deltaF;
-        deltaElement.classList.add('wynikiObl');
-        delta.appendChild(deltaElement);
+        dodajPole(`Miejsca zerowe funkcji`,`Funkcja ma dwa miejsca zerowe: x1=(${x1}) , x2=(${x2})`,miejscaZerowe);
 
-        const postacOgl = document.createElement('p');
-        postacOgl.innerHTML = `f(x) = ${a}x ${'<sup>2</sup>'} + ${b}x + ${c}`
-        postacOgl.classList.add('wynikiObl');
-        postacO.appendChild(postacOgl);
+        dodajPole(`Wierzchołek funkcji`,`W (${p} , ${q})`,wierzcholek);
 
-        const wierz = document.createElement('p');
-        wierz.innerHTML = `W (${p} , ${q})`
-        wierz.classList.add('wynikiObl');
-        wierzcholek.appendChild(wierz);
+        dodajPole(`Miejsce przecięcia z osią OY`,`Funkcja przecina oś OY w punkcie (0, ${c})`,przeciecieOY);
 
-        const postacKan = document.createElement('p');
-        postacKan.innerHTML = `f(x) = ${a}(x - (${p}) )${'<sup>2</sup>'} + (${q})`
-        postacKan.classList.add('wynikiObl');
-        postacK.appendChild(postacKan);
+    }else if( a!= 0 && deltaF==0){
+        dodajPole(`Postać ogólna funkcji`,`f(x) = ${a}x ${'<sup>2</sup>'} + ${b}x + ${c}`,postacO);
 
-        const postacIlo = document.createElement('p');
-        postacIlo.innerHTML = `f(x) = ${a}(x - (${x1}))*(x - (${x2}))`
-        postacIlo.classList.add('wynikiObl');
-        postacI.appendChild(postacIlo);
+        dodajPole(`Postać kanoniczna funkcji`,`f(x) = ${a}(x - (${p}) )${'<sup>2</sup>'}`,postacK);
 
-        const mZerowe = document.createElement('p');
-        mZerowe.innerHTML = `Funkcja ma dwa miejsca zerowe: x1=(${x1}) , x2=(${x2})`
-        mZerowe.classList.add('wynikiObl');
-        miejscaZerowe.appendChild(mZerowe);
+        dodajPole(`Postać iloczynowa funkcji`,`f(x) = ${a}(x - (${x0})${'<sup>2</sup>'})`,postacI);
 
-        const oY = document.createElement('p');
-        oY.innerHTML = `Funkcja przecina oś OY w punkcie (0, ${c})`
-        oY.classList.add('wynikiObl');
-        przeciecieOY.appendChild(oY);
+        dodajPole(`Delta`,deltaF,delta);
 
-    }else if( a!= "" && deltaF==0){
-        const postacIlo = document.createElement('p');
-        postacIlo.innerHTML = `f(x) = ${a}(x - (${x0})${'<sup>2</sup>'})`
-        postacIlo.classList.add('wynikiObl');
-        postacI.appendChild(postacIlo); 
+        dodajPole(`Miejsce zerowe funckji`,`Funkcja ma jedno miejsce zerowe: x0=(${x0})`,miejscaZerowe);
 
-        const mZerowe = document.createElement('p');
-        mZerowe.innerHTML = `Funkcja ma jedno miejsce zerowe: x0=(${x0})`
-        mZerowe.classList.add('wynikiObl');
-        miejscaZerowe.appendChild(mZerowe);
+        dodajPole(`Wierzchołek funkcji`,`W (${p} , ${q})`,wierzcholek);
 
-        const oY = document.createElement('p');
-        oY.innerHTML = `Funkcja przecina oś OY w punkcie (0, ${c})`
-        oY.classList.add('wynikiObl');
-        przeciecieOY.appendChild(oY);
+        dodajPole(`Miejsce przecięcia z osią OY`,`Funkcja przecina oś OY w punkcie (0, ${c})`,przeciecieOY);
 
-        const postacKan = document.createElement('p');
-        postacKan.innerHTML = `f(x) = ${a}(x - (${p}) )${'<sup>2</sup>'}`
-        postacKan.classList.add('wynikiObl');
-        postacK.appendChild(postacKan);
+    }else if( a!= 0 && deltaF<0){
+        dodajPole(`Postać ogólna funkcji`,`f(x) = ${a}x ${'<sup>2</sup>'} + ${b}x + ${c}`,postacO);
+ 
+        dodajPole(`Postać kanoniczna funkcji`,`f(x) = ${a}(x - (${p}) )${'<sup>2</sup>'} + (${q})`,postacK);
 
-        const wierz = document.createElement('p');
-        wierz.innerHTML = `W (${p} , ${q})`
-        wierz.classList.add('wynikiObl');
-        wierzcholek.appendChild(wierz);
+        dodajPole(`Delta`,deltaF,delta);
 
-        const deltaElement = document.createElement('p');
-        deltaElement.innerHTML = deltaF;
-        deltaElement.classList.add('wynikiObl');
-        delta.appendChild(deltaElement);
+        dodajPole(`Wierzchołek funkcji`,`W (${p} , ${q})`,wierzcholek);
 
-        const postacOgl = document.createElement('p');
-        postacOgl.innerHTML = `f(x) = ${a}x ${'<sup>2</sup>'} + ${b}x + ${c}`
-        postacOgl.classList.add('wynikiObl');
-        postacO.appendChild(postacOgl);
-
-    }else if( a!= "" && deltaF<0){
-        const deltaElement = document.createElement('p');
-        deltaElement.innerHTML = deltaF;
-        deltaElement.classList.add('wynikiObl');
-        delta.appendChild(deltaElement);
-
-        const postacOgl = document.createElement('p');
-        postacOgl.innerHTML = `f(x) = ${a}x ${'<sup>2</sup>'} + ${b}x + ${c}`
-        postacOgl.classList.add('wynikiObl');
-        postacO.appendChild(postacOgl);
-
-        const wierz = document.createElement('p');
-        wierz.innerHTML = `W (${p} , ${q})`
-        wierz.classList.add('wynikiObl');
-        wierzcholek.appendChild(wierz);
-
-        const postacKan = document.createElement('p');
-        postacKan.innerHTML = `f(x) = ${a}(x - (${p}) )${'<sup>2</sup>'} + (${q})`
-        postacKan.classList.add('wynikiObl');
-        postacK.appendChild(postacKan);
-
-        const postacIlo = document.createElement('p');
-        postacIlo.innerHTML = `Funkcja nie ma postaci iloczynowej`
-        postacIlo.classList.add('wynikiObl');
-        postacI.appendChild(postacIlo);
-
-        const mZerowe = document.createElement('p');
-        mZerowe.innerHTML = `Funkcja nie ma miejsc zerowych`
-        mZerowe.classList.add('wynikiObl');
-        miejscaZerowe.appendChild(mZerowe);
-
-        const oY = document.createElement('p');
-        oY.innerHTML = `Funkcja przecina oś OY w punkcie (0, ${c})`
-        oY.classList.add('wynikiObl');
-        przeciecieOY.appendChild(oY);
+        dodajPole(`Miejsce przecięcia z osią OY`,`Funkcja przecina oś OY w punkcie (0, ${c})`,przeciecieOY);
     };
 };
 
-
-
-
-
 function obrot(){
-    document.querySelector('.funkcjaK').style.transform = 'rotateY(-180deg)';
-    document.querySelector('.wyniki').style.transform = 'rotateY(0deg)';
-};
-function obrot1(){
-    location.reload();
+    if(!isNaN(wsplA.value) && !isNaN(wsplB.value) && !isNaN(wsplC.value)){
+        document.querySelector('.funkcjaK').style.transform = 'rotateY(-180deg)';
+        document.querySelector('.wyniki').style.transform = 'rotateY(0deg)';
+    }else{
+        return;
+    };
 
 };
+
+
+function dodajPole(Naglowek,Tresc,Pole){
+    const Ngl = document.createElement('p');
+    const El = document.createElement('p');
+    Ngl.innerHTML = Naglowek;
+    El.innerHTML = Tresc;
+    Ngl.classList.add('wynikiNaglowek');
+    El.classList.add('wynikiObl');
+    Pole.appendChild(Ngl);
+    Pole.appendChild(El);
+};
+
+
+function wroc(){
+    document.querySelector('.funkcjaK').style.transform = 'rotateY(0deg)';
+    document.querySelector('.wyniki').style.transform = 'rotateY(180deg)';
+
+    usun()
+
+};
+
+function usun(){
+    postacO.removeChild(document.querySelector('.wynikiNaglowek'));
+    postacO.removeChild(document.querySelector('.wynikiObl'));
+
+    postacK.removeChild(document.querySelector('.wynikiNaglowek'));
+    postacK.removeChild(document.querySelector('.wynikiObl'));
+
+    postacI.removeChild(document.querySelector('.wynikiNaglowek'));
+    postacI.removeChild(document.querySelector('.wynikiObl'));
+
+    delta.removeChild(document.querySelector('.wynikiNaglowek'));
+    delta.removeChild(document.querySelector('.wynikiObl'));
+
+    miejscaZerowe.removeChild(document.querySelector('.wynikiNaglowek'));
+    miejscaZerowe.removeChild(document.querySelector('.wynikiObl'));
+
+    wierzcholek.removeChild(document.querySelector('.wynikiNaglowek'));
+    wierzcholek.removeChild(document.querySelector('.wynikiObl'));
+
+    przeciecieOY.removeChild(document.querySelector('.wynikiNaglowek'));
+    przeciecieOY.removeChild(document.querySelector('.wynikiObl'));
+    
+}
 
 oblButton.addEventListener('click', obliczeniaFunkcji);
 oblButton.addEventListener('click',obrot);
-wrcButton.addEventListener('click',obrot1);
-
-
+wrcButton.addEventListener('click',wroc);
